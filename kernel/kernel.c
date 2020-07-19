5f517cc1e5b2667717abf88ade478b38ff1b74d2
Minnerlas <nikilirikili@gmail.com>
2020-07-19 22:35:34 +0200

    Manje izmene

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -24,6 +24,11 @@ void kernel_main(void) {
 	//	terminal_putchar('\n');
 	//	terminal_writestring(" Hello, kernel World! test \ntest test\n");
 	//}
+	//
+	for(int i = 0; i<28; i++) {
+		kprintf("%d\n Hello, kernel World! test \ntest test%d\n", i, i);
+	}
+
 	terminal_writestring("Ovo je test\n");
 	kprintf("TEST %d\n", 42);
 	kprintf("TEST %p\n", "test");
